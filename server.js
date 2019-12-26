9c337afcbdc59fdda51ec5685d798508d1bb963f
Nithin Pingili <nithinp300@gmail.com>
2019-12-25 22:49:54 -0600

    used API to display current weather

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index');
-})
+});
+
+const request = require('request');
+const apiKey = 'dd53df4128bb7c7716811875e6aef782';
 app.post('/', function (req, res) {
-  console.log(req.body.city);
-  res.render('index');
-})
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  request(url, function (err, response, body) {
+    if(err) {
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    }
+    else {
+      let weather = JSON.parse(body);
+      if(weather.main == undefined) {
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      }
+      else {
+        let weatherText = `It's ${weather.main.temp}°F in ${weather.name}`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
