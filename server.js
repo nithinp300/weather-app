764b5f8213fe23328d4a1e5c391234e38e4fee33
Nithin Pingili <nithinp300@gmail.com>
2019-12-25 19:21:48 -0600

    used Embedded Javascript using template engine for frontend

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
