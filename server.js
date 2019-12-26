0f272c1ea83d346eba9c55c542c6836a5ea6e305
Nithin Pingili <nithinp300@gmail.com>
2019-12-25 20:56:47 -0600

    post route set up

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 
-app.get('/', function (req, res) {
-  res.render('index')
-})
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
+app.get('/', function (req, res) {
+  res.render('index');
+})
+app.post('/', function (req, res) {
+  console.log(req.body.city);
+  res.render('index');
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
